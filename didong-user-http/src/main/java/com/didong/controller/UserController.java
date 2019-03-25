fb851e6ad8067a2548c076115277d3144b25020c
gongjian1990 <602192564@qq.com>
2019-03-25 11:53:11 +0800

    修改 pom ，新增test方法

--- didong-user-http/src/main/java/com/didong/controller/UserController.java
+++ didong-user-http/src/main/java/com/didong/controller/UserController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -18,6 +20,18 @@ public class UserController {
         String s = userService.postRestTemplate("user/test","55",String.class);
         System.out.println("s-----"+s);
         return "";
+    }
+
+
+    @RequestMapping("/authWX")
+    public String authWX(Map map){
+
+        String test = (String) map.get("test");
+
+        System.out.println("test--"+test);
+
+
+        return null;
     }
 
 }
